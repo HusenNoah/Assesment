0ad36c17dfe080a62d687a8fb5608482157a95ed
xuseennoah <lect2017@gmail.com>
2017-11-10 09:48:56 -0800

    i commit all

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var getsum = MyCheck();
+            Console.WriteLine(getsum);
+        }
+
+
+       public static int MyCheck()
+        {
+            const int Max = 4000000;
+            int sum = 0;
+            int a = 1;
+            int b = 1;
+            while (b < Max)
+            {
+                a += b;
+                b += a;
+                if( a%2==0 && a < Max)
+                {
+                    sum += a;
+
+                }
+                else if(b%2==0 && b < Max)
+                {
+                    sum += b;
+                }
+
+               
+            }
+            return sum;
+        }
+    }
+}
